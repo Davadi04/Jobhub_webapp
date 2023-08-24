53d20f265f71c01a6c578ca067b2f949820bbf42
Akingz2000 <seyiakintan@yahoo.com>
2023-08-24 12:56:31 -0700

    Backend

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import userRoute from "./routes/users.js";
 import JobsRoute from "./routes/jobs.js";
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000/", "https://jobhub-mern-app.onrender.com"],
+    origin: ["http://localhost:3000", "https://jobhubwebapp.vercel.app"],
   })
 );
 
@@ -31,8 +32,6 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() =>
-    app.listen(process.env.PORT, () =>
-      console.log(`Server Running on port :${5000}`)
-    )
+    app.listen(PORT, () => console.log(`Server Running on port :${5000}`))
   )
   .catch((error) => console.log(error.message));
